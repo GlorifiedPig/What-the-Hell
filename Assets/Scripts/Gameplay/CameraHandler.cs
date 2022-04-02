5403765182192d33f1c3283bcba6bfb1cd7200e3
Ryan <ryan@glorifiedstudios.com>
2022-04-02 19:44:56 +0200

    camera following mouse

--- Assets/Scripts/Gameplay/CameraHandler.cs
+++ Assets/Scripts/Gameplay/CameraHandler.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class CameraHandler : MonoBehaviour
 {
+    public Camera cam;
     public Transform player;
     public float smoothingSpeed = 0.125f;
+    public float maxDistance = 15f;
+    public float offsetZ = -10;
+
+    private Vector3 smoothingVelocity;
+    private Vector3 targetPosition;
 
     private void Update()
     {
-        Vector3 position = player.position;
-        position.z = transform.position.z;
-        transform.position = position;
+        Vector3 mousePos = cam.ScreenToWorldPoint( Input.mousePosition );
+        Vector3 position = ( player.position + mousePos ) / 2f;
+
+        position.x = Mathf.Clamp( position.x, -maxDistance + player.position.x, maxDistance + player.position.x );
+        position.y = Mathf.Clamp( position.y, -maxDistance + player.position.y, maxDistance + player.position.y );
+
+        position.z = offsetZ;
+        targetPosition = position;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp( transform.position, targetPosition, ref smoothingVelocity, smoothingSpeed );
     }
 }
