f72cc29ccb4fb18d25ebeddbbce123e2d45fba48
Ryan <ryan@glorifiedstudios.com>
2022-04-02 18:55:37 +0200

    smooth movement

--- Assets/Scripts/Gameplay/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 25f;
+    public float movementSpeed = 30f;
+    public float movementSmoothing = 0.12f;
     public Rigidbody2D rigidbody2d;
 
     private Vector2 cachedMovement;
+    private Vector2 movementVelocity;
 
     private void Update()
     {
-        cachedMovement.x = Input.GetAxisRaw( "Horizontal" );
-        cachedMovement.y = Input.GetAxisRaw( "Vertical" );
+        Vector2 input = new Vector2( Input.GetAxisRaw( "Horizontal" ), Input.GetAxisRaw( "Vertical" ) );
+        cachedMovement = Vector2.SmoothDamp( cachedMovement, input, ref movementVelocity, movementSmoothing );
     }
 
     private void FixedUpdate()
