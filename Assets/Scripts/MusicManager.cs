b1668d8c9aa4c4fb961cb51cd28246392ebd4e54
Ryan <ryan@glorifiedstudios.com>
2022-04-04 21:23:30 +0200

    background music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] music;
+    public AudioSource musicPlayer;
+    public float musicVolume = 0.35f;
+
+    public int musicState = 0;
+
+    int musicStateTest = -1;
+
+    public float transitionTime = 0.2f;
+
+    bool switchingMusic = false;
+
+
+    void PlayAppropriateMusic()
+    {
+        StartCoroutine( SwitchMusic( music[Random.Range( 0, music.Length - 1 )] ) );
+    }
+
+    void Update()
+    {
+        if( !switchingMusic )
+        {
+            if( musicStateTest != musicState )
+            {
+                musicStateTest = musicState;
+                PlayAppropriateMusic();
+            } else if( !musicPlayer.isPlaying ) PlayAppropriateMusic();
+        }
+    }
+
+    IEnumerator SwitchMusic( AudioClip nextSong )
+    {
+        switchingMusic = true;
+        float musicVol = 1;
+
+        while( musicVol > 0 )
+        {
+            musicVol = Mathf.Max( musicVol - ( Time.deltaTime / transitionTime ), 0 );
+            musicPlayer.volume = musicVol * musicVolume;
+            yield return new WaitForEndOfFrame();
+        }
+
+        musicPlayer.Stop();
+        musicPlayer.clip = nextSong;
+        musicPlayer.Play();
+
+        while( musicVol < 1 )
+        {
+            musicVol = Mathf.Min( musicVol + ( Time.deltaTime / transitionTime ), 1 );
+            musicPlayer.volume = musicVol * musicVolume;
+            yield return new WaitForEndOfFrame();
+        }
+
+        switchingMusic = false;
+    }
+}
