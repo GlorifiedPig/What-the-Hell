ed7dcc93b72a6274d552aec3c65942cbaa9460b9
Ryan <ryan@glorifiedstudios.com>
2022-04-04 22:30:47 +0200

    zombie spawning

--- Assets/Scripts/Gameplay/ZombieSpawnManager.cs
+++ Assets/Scripts/Gameplay/ZombieSpawnManager.cs
@@ -13,11 +13,11 @@ public class ZombieSpawnManager : MonoBehaviour
         InvokeRepeating( nameof( SpawnZombie ), spawnTime, spawnTime );
     }
 
-    public void SpawnZombie()
+    public GameObject SpawnZombie()
     {
         GameObject zombiePrefab = zombies[Random.Range( 0, zombies.Length )];
         Vector3 spawnPos = spawners[Random.Range( 0, spawners.Length )].position;
         spawnPos.z = zombiePrefab.transform.position.z;
-        Instantiate( zombiePrefab, spawnPos, Quaternion.identity );
+        return Instantiate( zombiePrefab, spawnPos, Quaternion.identity );
     }
 }
